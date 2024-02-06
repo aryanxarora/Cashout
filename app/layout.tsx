0edce52c33065ae360ad1ceb07ee37f8757b9bcf
Aryan Arora <aryanxarora@gmail.com>
2024-02-06 17:52:05 +0000

    Manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="manifest" href="/public/manifest.json" />
       <body suppressHydrationWarning={true}>
         {/* <Navbar /> */}
         <div className="hidden sm:block">
